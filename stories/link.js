524dffb7c51269842bc01f28dc5d724d5a84e3fb
Butt <anam.f.butt@gmail.com>
2020-10-27 14:23:38 +0100

    commit

--- stories/link.js
+++ stories/link.js
@@ -1,1 +1,11 @@
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+
+export default {
+    title: 'Link',
+    argTypes: {
+      children: { control: 'text' },
+    },
+  };
+  
 <a href="https://github.com/polyx/web-components-aspire">Design System</a>
