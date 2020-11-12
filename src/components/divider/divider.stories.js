9757ba926770663ec08043a782af896769f407de
noraro <nora-lyngstad.roste@capgemini.com>
2020-11-12 08:54:31 +0100

    added knobs

--- src/components/divider/divider.stories.js
+++ src/components/divider/divider.stories.js
@@ -1,7 +1,15 @@
 import { storiesOf } from '@storybook/html';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 storiesOf('Divider', module)
+  .addDecorator(withKnobs)
+  .add('Variant small', () => '<my-divider color="lighter" variant="small"></my-divider> <my-divider color="light" variant="small"></my-divider> <my-divider color="medium" variant="small"></my-divider>')
+  .add('Variant medium', () => '<my-divider color="lighter" variant="medium"></my-divider> <my-divider color="light" variant="medium"></my-divider><my-divider color="medium" variant="medium"></my-divider>')
+  .add('Knobs',() => {
+  const knobTest = document.createElement('my-divider');
+  knobTest.variant = text('variant', 'medium');
+  knobTest.color = text('color', 'medium');
 
-  .add('Variant small', () => '<my-divider color="lighter" variant="small"></my-divider> <my-divider color="light" variant="small"></my-divider> <my-divider color="medium" variant="small"></my-divider>'
-  )
-  .add('Variant medium', () => '<my-divider color="lighter" variant="medium"></my-divider> <my-divider color="light" variant="medium"></my-divider><my-divider color="medium" variant="medium"></my-divider>');
+  return knobTest;
+}
+);
