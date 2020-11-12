0442cd6ed9743586bbf484c73512c102d34a9112
noraroste <nora.roste@live.com>
2020-11-12 10:19:09 +0100

    Added text between dividers for more clear visualization

--- src/components/divider/divider.stories.js
+++ src/components/divider/divider.stories.js
@@ -4,12 +4,18 @@ import { withKnobs,  select} from '@storybook/addon-knobs';
 
 storiesOf('Divider', module)
   .addDecorator(withKnobs)
-  .add('Variant small', () => '<my-divider color="lighter" variant="small"></my-divider> <my-divider color="light" variant="small"></my-divider> <my-divider color="medium" variant="small"></my-divider>')
-  .add('Variant medium', () => '<my-divider color="lighter" variant="medium"></my-divider> <my-divider color="light" variant="medium"></my-divider><my-divider color="medium" variant="medium"></my-divider>')
+  .add('Variant small', () => 'text <my-divider color="lighter" variant="small"></my-divider> text <my-divider color="light" variant="small"></my-divider> text <my-divider color="medium" variant="small"></my-divider> text')
+  .add('Variant medium', () => 'text <my-divider color="lighter" variant="medium"></my-divider> text <my-divider color="light" variant="medium"></my-divider> text <my-divider color="medium" variant="medium"></my-divider> text')
   .add('Knobs',() => {
-  const knobTest = document.createElement('my-divider');
-  knobTest.variant = select('Variant', ['small', 'medium']);
-  knobTest.color = select('Color', ['medium','light','lighter']);
+  const knobTest = document.createElement('div');
+  const myDivider = document.createElement('my-divider');
+  myDivider.variant = select('Variant', ['small', 'medium']);
+  myDivider.color = select('Color', ['medium','light','lighter']);
+  const text1 = document.createTextNode("This is some text");
+  const text2 = document.createTextNode("This is some more text");
+  knobTest.appendChild(text1);
+  knobTest.appendChild(myDivider);
+  knobTest.appendChild(text2);
 
   return knobTest;
 }
