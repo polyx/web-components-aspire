b1cda2ee3a6acb520cf2528e5a9e8c0c10e57345
noraroste <nora.roste@live.com>
2020-11-13 10:18:36 +0100

    Added notes for divider + name improvement

--- src/components/divider/divider.stories.js
+++ src/components/divider/divider.stories.js
@@ -1,14 +1,25 @@
 import { storiesOf } from '@storybook/html';
 import { withKnobs,  select} from '@storybook/addon-knobs';
 
+import readme from './readme.md';
 
 storiesOf('Divider', module)
   .addDecorator(withKnobs)
-  .add('Variant small', () => 'text <my-divider color="lighter" variant="small"></my-divider> text <my-divider color="light" variant="small"></my-divider> text <my-divider color="medium" variant="small"></my-divider> text')
-  .add('Variant medium', () => 'text <my-divider color="lighter" variant="medium"></my-divider> text <my-divider color="light" variant="medium"></my-divider> text <my-divider color="medium" variant="medium"></my-divider> text')
+  .add('Variant small', () => 'text <esp-divider color="lighter" variant="small"></esp-divider> text <esp-divider color="light" variant="small"></esp-divider> text <esp-divider color="medium" variant="small"></esp-divider> text',
+  {
+      notes: {
+        markdown: readme,
+      },
+    })
+  .add('Variant medium', () => 'text <esp-divider color="lighter" variant="medium"></esp-divider> text <esp-divider color="light" variant="medium"></esp-divider> text <esp-divider color="medium" variant="medium"></esp-divider> text',
+  {
+      notes: {
+        markdown: readme,
+      },
+    })
   .add('Knobs',() => {
   const knobTest = document.createElement('div');
-  const myDivider = document.createElement('my-divider');
+  const myDivider = document.createElement('esp-divider');
   myDivider.variant = select('Variant', ['small', 'medium']);
   myDivider.color = select('Color', ['medium','light','lighter']);
   const text1 = document.createTextNode("This is some text");
@@ -18,5 +29,11 @@ storiesOf('Divider', module)
   knobTest.appendChild(text2);
 
   return knobTest;
-}
+},
+{
+      notes: {
+        markdown: readme,
+      },
+    }
+
 );
--- src/components/divider/divider.tsx
+++ src/components/divider/divider.tsx
@@ -1,7 +1,7 @@
 import {Component, Prop, h} from '@stencil/core'
 
 @Component({
-tag:'my-divider',
+tag:'esp-divider',
 styleUrl: 'divider.css',
 shadow: true
 })
